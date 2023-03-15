21a7d466634ebd26e9cb41b57e6b840ccb73bbac
martinvidela <videla116@gmail.com>
2023-03-14 21:05:29 -0300

    finallydone

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Clickbutton.forEach(btn => {
   btn.addEventListener('click', addToCarritoItem)
 })
 
-
+//FUNCION PARA AGREGAR AL CARRO
 function addToCarritoItem(e){
   const button = e.target
   const item = button.closest('.card')
@@ -25,14 +25,24 @@ function addToCarritoItem(e){
 }
 
 
-const InputElemento = tbody.getElementsByClassName('input__elemento')
 function addItemCarrito(newItem){
 
+const alert = document.querySelector('.alert')
+
+setTimeout(function(){
+  alert.classList.add('hide')
+}, 2000)
+alert.classList.remove('hide')
+
+
+
+  const InputElemento = tbody.getElementsByClassName('input__elemento')
     for(let i = 0; i < carrito.length; i++){
         if(carrito[i].title.trim() === newItem.title.trim()){
             carrito[i].cantidad ++;
             const inputValue = InputElemento[i]
             inputValue.value++
+            CarritoTotal()
             return null;
         }
     }
@@ -41,7 +51,7 @@ function addItemCarrito(newItem){
      
     renderCarrito()
 }
-
+//RENDER
 function renderCarrito(){
     tbody.innerHTML = ''
     carrito.map(item => {
@@ -63,10 +73,62 @@ function renderCarrito(){
       `
       tr.innerHTML = Content;
       tbody.append(tr)
-    })
-}
 
+      tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+      tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+    })
+    CarritoTotal()
+}
+//TOTAL DEL CARRITO
 function CarritoTotal(){
     let Total = 0;
-    const itemCartTotal = 
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    carrito.forEach((item) =>{
+      const precio = Number(item.precio.replace("$", ''))
+      Total = Total + precio*item.cantidad
+    })
+
+    itemCartTotal.innerHTML = `Total $${Total}`
+    addLocalStorage()
+}
+//REMOVER ITEM DEL CARRITO
+function removeItemCarrito(e){
+const buttonDelete = e.target
+const tr = buttonDelete.closest (".ItemCarrito")
+const title = tr.querySelector(".title").textContent;
+for(let i=0; i<carrito.length; i++){
+  
+  if(carrito[i].title.trim() === title.trim()){
+    carrito.splice(i,1)
+  }
+}
+tr.remove()
+CarritoTotal()
+}
+//SUMA DE PRECIO SI CAMBIA EL VALUE
+function sumaCantidad(e){
+  const sumaInput  = e.target
+  const tr = sumaInput.closest(".ItemCarrito")
+  const title = tr.querySelector('.title').textContent;
+  carrito.forEach(item => {
+    if(item.title.trim() === title){
+      sumaInput.value < 1 ?  (sumaInput.value = 1) : sumaInput.value;
+      item.cantidad = sumaInput.value;
+      CarritoTotal()
+    }
+  })
+}
+
+function addLocalStorage(){
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+
+//LOCALSTORAGE
+window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem('carrito'));
+  if(storage){
+    carrito = storage;
+    renderCarrito()
+  }
 }
